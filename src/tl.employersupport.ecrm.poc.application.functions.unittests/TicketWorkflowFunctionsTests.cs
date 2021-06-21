ff4b59b94a86fee207f17b06bb14d820dda92896
Mike Wild <wildconsultingltd@googlemail.com>
2021-06-21 14:53:05 +0100

    Add test for modify tags

--- src/tl.employersupport.ecrm.poc.application.functions.unittests/TicketWorkflowFunctionsTests.cs
+++ src/tl.employersupport.ecrm.poc.application.functions.unittests/TicketWorkflowFunctionsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
@@ -40,7 +41,7 @@ namespace tl.employersupport.ecrm.poc.application.functions.unittests
         public async Task TicketWorkflowFunctions_SendTicketCreatedNotification_Via_Get_Returns_Expected_Result()
         {
             const long ticketId = 4485;
-            var createdTime = DateTime.Parse("10:10:57");
+            var createdTime = DateTime.Parse("2021-06-15 10:10:57");
 
             var emailService = Substitute.For<IEmailService>();
             emailService
@@ -74,7 +75,7 @@ namespace tl.employersupport.ecrm.poc.application.functions.unittests
         public async Task TicketWorkflowFunctions_SendTicketCreatedNotification_Via_Post_Returns_Expected_Result()
         {
             const long ticketId = 4485;
-            var createdTime = DateTime.Parse("10:10:57");
+            var createdTime = DateTime.Parse("2021-06-15 10:10:57");
 
             var emailService = Substitute.For<IEmailService>();
             emailService
@@ -118,10 +119,10 @@ namespace tl.employersupport.ecrm.poc.application.functions.unittests
         }
 
         [Fact]
-        public async Task TicketWorkflowFunctions_SendTicketCreatedNotification_With_No_data_Via_Get_Returns_Bad_Request_Result()
+        public async Task TicketWorkflowFunctions_SendTicketCreatedNotification_With_No_Data_Via_Get_Returns_Bad_Request_Result()
         {
             const long ticketId = 4485;
-            var createdTime = DateTime.Parse("10:10:57");
+            var createdTime = DateTime.Parse("2021-06-15 10:10:57");
 
             var emailService = Substitute.For<IEmailService>();
             emailService
@@ -149,6 +150,61 @@ namespace tl.employersupport.ecrm.poc.application.functions.unittests
             await emailService
                 .DidNotReceive()
                 .SendZendeskTicketCreatedEmail(Arg.Any<long>(), Arg.Any<DateTime>());
+        }
+
+        [Fact]
+        public async Task TicketWorkflowFunctions_ModifyTags_Returns_Expected_Result()
+        {
+            const long ticketId = 4485;
+            var updatedTime = DateTime.Parse("2021-06-15 10:10:57");
+
+            var existingTags = new List<string>()
+            {
+                "1-9__micro_",
+                "customer",
+                "i_m_ready_to_offer_an_industry_placement",
+                "tlevels-email",
+                "tlevels_approved"
+            };
+
+            var ticketService = Substitute.For<ITicketService>();
+            ticketService
+                .GetTicketTags(ticketId)
+                .Returns(new SafeTags {
+                    Tags = existingTags, 
+                    UpdatedStamp = new DateTimeOffset(updatedTime)
+                });
+
+            var modifyTagsRequest = new ModifyTagsMessage
+            {
+                TicketId = ticketId,
+                Tags = existingTags
+            };
+
+            var json = JsonSerializer.Serialize(modifyTagsRequest,
+                new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+
+            var request = FunctionObjectsBuilder
+                .BuildHttpRequestData(
+                    HttpMethod.Post,
+                    $"https://test.com/ModifyZendeskTicketTags",
+                    json);
+
+            var functionContext = FunctionObjectsBuilder.BuildFunctionContext();
+            
+            var functions = new TicketWorkflowFunctionsBuilder()
+                .Build(ticketService: ticketService);
+
+            var result = await functions.ModifyZendeskTicketTags(request, functionContext);
+
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            await ticketService
+                .Received(1)
+                .GetTicketTags(ticketId);
         }
     }
 }
--- src/tl.employersupport.ecrm.poc.application/Model/Zendesk/ModifyTagsMessage.cs
+++ src/tl.employersupport.ecrm.poc.application/Model/Zendesk/ModifyTagsMessage.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace tl.employersupport.ecrm.poc.application.Model.Zendesk
 {
