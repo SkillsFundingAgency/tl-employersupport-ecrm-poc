f5241af5b8f47bf51f4cd9d64a107783d88ee1e7
Mike Wild <wildconsultingltd@googlemail.com>
2021-06-30 15:15:09 +0100

    Set web host proxy in dev to avoid startup error

--- src/tl.employersupport.ecrm.poc.application.functions/Program.cs
+++ src/tl.employersupport.ecrm.poc.application.functions/Program.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using tl.employersupport.ecrm.poc.application.functions.Extensions;
@@ -6,21 +8,27 @@ using tl.employersupport.ecrm.poc.application.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices((_, services) =>
+    .ConfigureServices((context, services) =>
+    {
+        if (context.HostingEnvironment.IsDevelopment())
         {
-            //Required order - add configuration first, then http clients, then other services
-            services
-                .AddEmailConfiguration()
-                .AddEcrmConfiguration()
-                .AddZendeskConfiguration()
-                .AddEcrmHttpClient()
-                .AddZendeskHttpClient()
-                .AddTransient<IDateTimeService, DateTimeService>()
-                .AddEmailServices()
-                .AddTransient<ITicketService, TicketService>()
-                .AddTransient<IEcrmService, EcrmService>()
-                ;
-        })
+            //https://stackoverflow.com/questions/66500195/net-5-grpc-client-call-throws-exception-requesting-http-version-2-0-with-versi
+            HttpClient.DefaultProxy = new WebProxy();
+        }
+
+        //Required order - add configuration first, then http clients, then other services
+        services
+            .AddEmailConfiguration()
+            .AddEcrmConfiguration()
+            .AddZendeskConfiguration()
+            .AddEcrmHttpClient()
+            .AddZendeskHttpClient()
+            .AddTransient<IDateTimeService, DateTimeService>()
+            .AddEmailServices()
+            .AddTransient<ITicketService, TicketService>()
+            .AddTransient<IEcrmService, EcrmService>()
+            ;
+    })
     .Build();
 
 host.Run();
