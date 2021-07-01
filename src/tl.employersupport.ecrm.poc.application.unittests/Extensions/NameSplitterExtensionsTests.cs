29c6b2bb459b497ec163b1e399700f13453685aa
Mike Wild <wildconsultingltd@googlemail.com>
2021-07-01 12:52:41 +0100

    Add name splitter and tests

---
+++ src/tl.employersupport.ecrm.poc.application.unittests/Extensions/NameSplitterExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using tl.employersupport.ecrm.poc.application.Extensions;
+using Xunit;
+
+namespace tl.employersupport.ecrm.poc.application.unittests.Extensions
+{
+    public class NameSplitterExtensionsTests
+    {
+        [Theory(DisplayName = nameof(NameSplitterExtensions) + " Data Tests")]
+        [InlineData("Bob Bobs", "Bob", "Bobs")]
+        [InlineData("Bob", "Bob", "")]
+        [InlineData("Corporal Kevin Swells", "Kevin", "Swells")]
+        [InlineData("Mrs. Joan Johnson", "Joan", "Johnson")]
+        public void NameSplitterExtensions_SplitName_Returns_Expected_Results(string input, string first, string last)
+        {
+            var result = input.SplitName();
+            result.First.Should().Be(first);
+            result.Last.Should().Be(last);
+        }
+    }
+}
---
+++ src/tl.employersupport.ecrm.poc.application/Extensions/NameSplitterExtensions.cs
@@ -0,0 +1,13 @@
+﻿using NameParser;
+
+namespace tl.employersupport.ecrm.poc.application.Extensions
+{
+    public static class NameSplitterExtensions
+    {
+        public static (string First, string Last) SplitName(this string name)
+        {
+            var nameParser = new HumanName(name);
+            return (nameParser.First, nameParser.Last);
+        }
+    }
+}
