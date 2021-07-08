2dffce17f92a319d578f5cc42c78a4ac05285a87
Mike Wild <wildconsultingltd@googlemail.com>
2021-07-08 16:16:32 +0100

    Tidy up provider search POC code

--- src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
+++ src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ess_providersearch_poc.Pages
+namespace tl.ess.providersearch.poc
 {
     public class IndexModel : PageModel
     {
--- src/tl.ess.providersearch.poc/README.md
+++ src/tl.ess.providersearch.poc/README.md
@@ -1,7 +1,7 @@
-﻿
+﻿# ESS Provider Search POC
 
 ## TODO
 
 - Get postcode from form
 - Get remote url from config via injected model
-- 
+- fill in this readme
