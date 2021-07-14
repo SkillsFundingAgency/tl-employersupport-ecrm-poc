537d0bee9e4a16c05bcc9226d17c19bc9d97de47
Mike Wild <wildconsultingltd@googlemail.com>
2021-07-14 15:46:44 +0100

    Finish zendesk provider search poc

---
+++ src/tl.ess.providersearch.poc/wwwroot/js/site.js
@@ -0,0 +1,91 @@
+﻿$("#tl-search-providers").click(function () {
+    event.stopPropagation();
+
+    $("#tl-provider-search-status").html("searching...");
+    $('#tl-provider-search-status').removeClass("tl-hidden");
+
+    //TODO: Get postcode from form
+    const postcode = 'OX2 9GX';
+    let webApiUrl = 'https://localhost:58879/api/search';
+    webApiUrl = $('#searchApiUrlValue').val();
+    console.log(`webApiUrl is ${webApiUrl}`);
+
+    const search = new URLSearchParams({
+        postcode: postcode,
+    }).toString();
+
+    console.log(`Ready to call ${webApiUrl}?${search}`);
+
+    const request = new Request(`${webApiUrl}?${search}`);
+
+    fetch(request)
+        .then(response => response.json())
+        //.then(data => console.log(data));
+        .then(function (data) {
+            console.log('Request succeeded with JSON response:', data);
+            populateSearchResults(data);            
+        }).catch(function (error) {
+            console.log('Request failed:', error.message);
+        })
+});
+
+function populateSearchResults(data) {
+
+    $("#tl-provider-search-status").html("loading...");
+
+    $("#tl-provider-search-results-json").html(JSON.stringify(data, undefined, 2));
+
+    let searchResults =
+        '<div class="tl-results"> \
+            <p>Qualifications:</p> \
+            <ul>';
+
+    //List qualifications
+    data.qualifications.forEach(function (item) {
+        searchResults += `<li>${item.text} (${item.value})</li>`
+    });
+
+    searchResults +=
+        '</ul> \
+         <p>Providers:</p> \
+            <ul>';
+
+    data.providerLocations.forEach(function (item) {
+        searchResults +=
+            `<div class="tl-results-box"> \
+                <h3><span class="tl-results-box--distance">${item.distanceInMiles} miles</span> \
+                    ${item.providerName}</h3> \
+                    <h4>${item.postcode}</h4>`;
+
+        item.deliveryYears.forEach(function (year) {
+            searchResults +=
+                `<h5>From ${year.year}</h5> \
+                   <ul>`;
+
+            year.qualifications.forEach(function (qualification) {
+                searchResults +=
+                    `<li>${qualification.name}</li>`;
+            });
+
+            searchResults += '</ul>';
+        });
+
+        searchResults += 
+            `<a target="_" href='${item.website}' class='tl-link'>Go to provider website</a> \
+             <a target="_" href='${item.journeyUrl}' class='tl-link'>How to get there</a>`;
+
+        searchResults += '</div >';
+    });
+
+    searchResults += '</ul">'
+
+    searchResults += '</div">'
+
+    console.log(searchResults);
+
+    $("#tl-provider-search-results").empty();
+    $("#tl-provider-search-results").append(searchResults);
+
+    $("#tl-provider-search-status").html("");
+    $('#tl-provider-search-status').addClass("tl-hidden");
+}
