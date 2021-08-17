62072f7fd50c9fbb30d439557503272db6f0b63f
Mike Wild <wildconsultingltd@googlemail.com>
2021-08-17 16:49:07 +0100

    Update web project to latest .net 6 version

--- src/tl.ess.providersearch.poc/Program.cs
+++ src/tl.ess.providersearch.poc/Program.cs
@@ -1,11 +1,38 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using tl.ess.providersearch.poc;
 
-Host.CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.UseStartup<Startup>();
-    })
-    .Build()
-    .Run();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddRazorPages()
+    //.AddRazorRuntimeCompilation()
+    ;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
