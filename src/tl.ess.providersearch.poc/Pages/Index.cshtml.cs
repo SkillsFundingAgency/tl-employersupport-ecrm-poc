2942b61a4f0243f6e98cc59083cb00e02dc83c2c
Mike Wild <wildconsultingltd@googlemail.com>
2021-08-31 16:20:49 +0100

    Merge qualifications for Available Now years

--- src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
+++ src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
@@ -8,11 +8,13 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    public string BearerToken { get; init; }
     public string SearchApiUrl { get; init; }
-
+    
     public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
     {
         _logger = logger;
+        BearerToken = config["BearerToken"];
         SearchApiUrl = config["SearchApiUrl"];
     }
 
