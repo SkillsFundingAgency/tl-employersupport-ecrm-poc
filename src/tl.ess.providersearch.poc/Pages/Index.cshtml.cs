1b4519226f4cc399f0e184bf64e248630e6ceee7
Mike Wild <wildconsultingltd@googlemail.com>
2021-09-10 18:12:03 +0100

    Update to use API key

--- src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
+++ src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
@@ -8,12 +8,14 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    public string ApiKey { get; init; }
     public string BearerToken { get; init; }
     public string SearchApiUrl { get; init; }
     
     public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
     {
         _logger = logger;
+        ApiKey = config["ApiKey"];
         BearerToken = config["BearerToken"];
         SearchApiUrl = config["SearchApiUrl"];
     }
