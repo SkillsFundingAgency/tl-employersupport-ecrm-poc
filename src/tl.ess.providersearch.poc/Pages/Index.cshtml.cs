e09a77219bc5790aad7ca7eb08b22b9856fe5f2a
Mike Wild <wildconsultingltd@googlemail.com>
2021-09-14 13:09:47 +0100

    Minor fixes

--- src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
+++ src/tl.ess.providersearch.poc/Pages/Index.cshtml.cs
@@ -9,14 +9,12 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
 
     public string ApiKey { get; init; }
-    public string BearerToken { get; init; }
     public string SearchApiUrl { get; init; }
     
     public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
     {
         _logger = logger;
         ApiKey = config["ApiKey"];
-        BearerToken = config["BearerToken"];
         SearchApiUrl = config["SearchApiUrl"];
     }
 
